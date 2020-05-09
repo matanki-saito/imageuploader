951a7c7e2da6ff1eac22396813633e0ff1bcccff
matanki.saito <matanki.saito@gmail.com>
2020-05-10 08:31:29 +0900

    サムネイルを作る関数

---
+++ src/main/java/com/popush/imageuploader/common/utility/ImageUtility.java
@@ -0,0 +1,46 @@
+package com.popush.imageuploader.common.utility;
+
+import static java.awt.Image.SCALE_SMOOTH;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import javax.imageio.ImageIO;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ImageUtility {
+
+    /**
+     * @see <a href="https://stackoverflow.com/questions/1069095/how-do-you-create-a-thumbnail-image-out-of-a-jpeg-in-java">link</a>
+     */
+    public ByteBuffer generateThumbnail(ByteBuffer image, int maxWidth) throws IOException {
+
+        Image thumbnailImage;
+        try (var in = new ByteArrayInputStream(image.array())) {
+            thumbnailImage = ImageIO.read(in)
+                                    .getScaledInstance(maxWidth, -1, SCALE_SMOOTH);
+            thumbnailImage.flush();
+        }
+
+        BufferedImage bufferedImage = new BufferedImage(thumbnailImage.getWidth(null),
+                                                        thumbnailImage.getWidth(null),
+                                                        TYPE_INT_RGB);
+
+        bufferedImage.createGraphics().drawImage(thumbnailImage, 0, 0, null);
+
+        ByteBuffer result;
+        try (var out = new ByteArrayOutputStream()) {
+            ImageIO.write(bufferedImage, "jpg", out);
+            result = ByteBuffer.wrap(out.toByteArray());
+        }
+
+        return result;
+    }
+}
